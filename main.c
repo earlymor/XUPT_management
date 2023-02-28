2eec9349d27365ce829888d0c5a2ca21f31e3ad0
earlymor <1162907534@qq.com>
2023-02-28 20:35:03 +0800

    day1

--- main.c
+++ main.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>  //linux sleep()
+// #include<windows.h> sleep()
+#define FILENAME "/home/earlymor/VSCODE/c/XUPT_management/INFO.txt"
+#define SIZE 1000
 typedef struct Member {
     char name[30];        // 会员姓名
-    int grade;            // 会员卡等级
-    int order;            // 会员编号
+    unsigned int grade;   // 会员卡等级
+    unsigned int order;   // 会员编号
     char telenumber[12];  // 预留手机号
     int balance;          // 余额
-    int score;            // 积分
-    float permissions;    // 权限
+    unsigned int score;   // 积分
+    float permissions;    // 折扣
 
 } Member;
 
+struct Member people[SIZE];
+int num = 0;
 typedef struct Node {
     struct Member data;
     struct Node* next;
 } Node;
 
-void data_add(Node*);  // 增加数据
-void data_delete(Node*);  // 删除数据
-void data_find(Node*);    // 查找数据
-void data_modify(Node*);  // 修改数据
-void data_sort(Node*);  // 数据排序
-void data_print(Node*);  // 打印数据
-void data_save(Node*);  // 保存数据
-void data_account(Node*);  // 统计数据
-void menu();
-
-// void IO_ReadInfo();			//读取文件
+void data_add(Node* head);      // 增加数据
+void data_delete(Node* head);   // 删除数据
+Node* data_find(Node* head);    // 查找数据
+void data_modify(Node* head);   // 修改数据
+void data_sort(Node* head);     // 数据排序
+void data_print(Node* head);    // 打印数据
+void data_save(Node* head);     // 保存数据
+void data_account(Node* head);  // 统计数据
+void show_single(Node* p);      // 显示单条数据
+void menu();                    // 菜单
+Node* IO_ReadInfo();            // 读取文件
 
 int main() {
-    //  IO_ReadInfo();			//读取文件
-    Node* head = (Node*)malloc(sizeof(Node));
-    head->next = NULL;
+    Node* head = IO_ReadInfo();  // 读取文件
     while (1) {
         menu();
         printf("请选择1-8\n");
@@ -88,17 +92,13 @@ void menu() {
     printf("###  0.退出系统                        ###\n");
     printf("##########################################\n");
 }
-// void IO_ReadInfo(){/*读取file.txt*/
-//     FILE *fp;
-//     if((fp=fopen("file.txt",'r'))==NULL){
-//         printf("无法打开文件");
-//     }
-//     else {
-
-//     }
-// }
-
-void data_add(Node* head) {
+void show_single(Node* p) {
+    printf("会员卡等级\t会员编号\t会员姓名\t预留手机号\t余额\t积分\t折扣\n");
+    printf("%5d\t\t%08d\t%-16s\t%-11s\t %d\t %d\t%.1f\n", p->data.grade,
+           p->data.order, p->data.name, p->data.telenumber, p->data.balance,
+           p->data.score, p->data.permissions);
+}
+void data_add(Node* head) {  // 增加数据
     Node* p = head;
     while (p->next) {
         p = p->next;
@@ -124,11 +124,12 @@ void data_add(Node* head) {
     printf("请输入积分：\n");
     scanf("%d", &s->data.score);
     getchar();
-    printf("请输入权限：\n");
+    printf("请输入折扣：\n");
     scanf("%f", &s->data.permissions);
     getchar();
 }
-void data_delete(Node* head) {
+
+void data_delete(Node* head) {  // 删除数据
     printf("请输入要删除的会员序号：\n");
     int n;
     scanf("%d", &n);
@@ -149,23 +150,162 @@ void data_delete(Node* head) {
         sleep(2);
     }
 }
-void data_find(Node* head) {}
-void data_change(Node* head) {}
-void data_print(Node* head) {
-    printf("会员卡等级\t会员编号\t会员姓名\t预留手机号\t余额\t积分\t权限\n");
+Node* data_find(Node* head) {  // 查找数据
+    int n;
+    printf("请输入要查找的会员序号:\n");
+    scanf("%d", &n);
+    getchar();
+    Node* p = head;
+    while (p->next && p->data.order != n) {
+        p = p->next;
+    }
+    if (p->data.order == n) {
+        show_single(p);
+        printf("按回车键退出");
+        getchar();
+        return p;
+    } else {
+        printf("序号不存在\n");
+        sleep(2);
+        return NULL;
+    }
+}
+void data_print(Node* head) {  // 打印数据
+    printf("会员卡等级\t会员编号\t会员姓名\t预留手机号\t余额\t积分\t折扣\n");
     Node* p = head;
     p = p->next;
     while (p) {
-        printf("%5d\t\t%08d\t%-8s\t%-11s\t %d\t %d\t%.1f\n", p->data.grade,
+        printf("%5d\t\t%08d\t%-16s\t%-11s\t %d\t %d\t %.1f\n", p->data.grade,
                p->data.order, p->data.name, p->data.telenumber, p->data.balance,
                p->data.score, p->data.permissions);
         p = p->next;
     }
-    printf("按任意键退出");
+    printf("按回车键键退出");
     getchar();
 }
-void data_save(Node* head) {}
+void data_save(Node* head) {  // 保存数据
+    FILE* fp;
+    if ((fp = fopen(FILENAME, "wb")) == NULL) {
+        printf("不能打开文件!\n");
+        return;
+    } else {
+        Node* p = head;
+        p = p->next;
+        while (p) {
+            fprintf(fp, "%s %d %d %s %d %d %f\n", p->data.name, p->data.grade,
+                    p->data.order, p->data.telenumber, p->data.balance,
+                    p->data.score, p->data.permissions);
+            p = p->next;
+        }
+    }
+    fclose(fp);
+    printf("保存成功");
+}
 
-void data_account(Node* head) {}
-void data_modify(Node* head) {}
-void data_sort(Node* head) {}
+void data_account(Node* head) {  // 统计数据
+}
+void data_modify(Node* head) {  // 修改数据
+    system("clear");
+    Node* p;
+    int n, key = 1, key2 = 1;
+    if ((p = data_find(head)) == NULL) {
+        return;
+    } else {
+        while (key) {
+            key2=1;
+            printf("--------------------请选择要修改的数据------------\n");
+            printf("- 1.会员姓名                                     -\n");
+            printf("- 2.会员卡等级                                   -\n");
+            printf("- 3.预留手机号                                   -\n");
+            printf("- 4.余额                                         -\n");
+            printf("- 5.积分                                         -\n");
+            printf("- 6.折扣                                         -\n");
+            printf("- 0.退出                                         -\n");
+            printf("--------------------------------------------------\n");
+            scanf("%d", &n);
+            getchar();
+            switch (n) {
+                case 1:
+                    printf("将会员姓名修改为：\n");
+                    scanf("%s", &p->data.name);
+
+                    break;
+                case 2:
+                    printf("将会员卡等级修改为：\n");
+                    scanf("%d", &p->data.grade);
+
+                    break;
+                case 3:
+                    printf("将预留手机号修改为：\n");
+                    scanf("%s", &p->data.telenumber);
+
+                    break;
+                case 4:
+                    printf("将余额修改为：\n");
+                    scanf("%d", &p->data.balance);
+
+                    break;
+                case 5:
+                    printf("将积分修改为：\n");
+                    scanf("%d", &p->data.score);
+
+                    break;
+                case 6:
+                    printf("将折扣修改为：\n");
+                    scanf("%f", &p->data.permissions);
+
+                    break;
+                case 0:
+                    return;
+                default:
+                    printf("无此选项,请重新选择");
+                    break;
+            }
+            while (key2) {
+                fflush(stdin);
+                getchar();
+                show_single(p);
+                printf("是否继续修改:(Y or N)\n");
+                char ch;
+                scanf("%c", &ch);
+                if (ch == 'N' || ch == 'n') {
+                    key = 0;
+                    key2 = 0;
+                } else if (ch == 'Y' || ch == 'y' || ch == '\n') {
+                    system("clear");
+                    key2 = 0;
+                } else
+                    printf("无此选项,请重新选择");
+            }
+        }
+    }
+}
+void data_sort(Node* head) {  // 排序数据
+}
+Node* IO_ReadInfo() {  // 读取文件
+    FILE* fp;
+    int i;
+    Node *head, *p = (Node*)malloc(sizeof(Node));
+    head = p;
+    if ((fp = fopen(FILENAME, "r")) == NULL) {
+        printf("不能打开文件!\n");
+        return head;
+    } else {
+        while (!feof(fp)) {
+            Node man;
+            int end = fscanf(fp, "%s %d %d %s %d %d %f", &man.data.name,
+                             &man.data.grade, &man.data.order,
+                             &man.data.telenumber, &man.data.balance,
+                             &man.data.score, &man.data.permissions);
+            man.next = NULL;
+            if (end != -1) {
+                Node* s = (Node*)malloc(sizeof(Node));
+                *s = man;
+                p->next = s;
+                p = s;
+            }
+        }
+    }
+    fclose(fp);
+    return head;
+}
